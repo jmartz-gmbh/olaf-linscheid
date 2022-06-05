a97e08334b580efaea8904e090ff2afd81ef7864
Jonathan Martz <git@jmartz.de>
2022-06-05 09:40:56 +0200

    update homepage preview

--- app/src/pages/home.vue
+++ app/src/pages/home.vue
@@ -4,20 +4,22 @@
       <div class="col-span-6 md:col-span-3 text-center">
         <h2 class="font-bold text-lg mt-5">Willkommen</h2>
         <p>
-          auf der neuen Webseite des Come In's. <br />
-          Hier findest du alle Informationen zu den Getränken <br />
-          m unserer Musik und unseren DJ's.
+          auf der neuen Webseite von der Firma Linscheid. <br />
+          Hier findest du alle Informationen zu unseren Servicen.
         </p>
       </div>
       <div class="col-span-6 md:col-span-3"></div>
       <div class="col-span-6">
         <hr class="mt-5 mb-5" />
       </div>
-      <div class="col-span-6 md:col-span-3">
-        <h2>Events</h2>
+      <div class="col-span-6 md:col-span-2">
+        <h2>Gas</h2>
       </div>
-      <div class="col-span-6 md:col-span-3">
-        <h2>Bilder</h2>
+      <div class="col-span-6 md:col-span-2">
+        <h2>Wasser</h2>
+      </div>
+      <div class="col-span-6 md:col-span-2">
+        <h2>Scheiße</h2>
       </div>
     </div>
   </div>
